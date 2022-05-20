4683c53e2661439f97aead3c0041e62e594e4353
Sasha7b9 <Sasha7b9@tut.by>
2022-05-20 20:50:25 +0300

    *

--- sources/TAPview/ParserTAP.cpp
+++ sources/TAPview/ParserTAP.cpp
@@ -5,6 +5,7 @@
 #include <wx/statline.h>
 #include <wx/file.h>
 #include <wx/wfstream.h>
+#include <wx/datstrm.h>
 #pragma warning (pop)
 
 
@@ -12,14 +13,78 @@ void ParserTAP::Run(wxBufferedInputStream &stream)
 {
     blocks.clear();
 
-    while (!stream.Eof())
+    wxDataInputStream data(stream);
+
+    BlockTAP block;
+
+    while (data.IsOk() && block.Parse(data))
     {
-        blocks.push_back(ParseBlock(stream));
+        blocks.push_back(block);
+    }
+
+    if (data.IsOk() || !block.IsValid())
+    {
+        blocks.clear();
     }
 }
 
 
-BlockTAP ParserTAP::ParseBlock(wxBufferedInputStream &stream)
+bool BlockTAP::Parse(wxDataInputStream &stream)
 {
+    Clear();
 
+    if (header.Parse(stream))
+    {
+        data.Parse(stream);
+    }
+
+    return header.IsValid() && data.IsValid();
+}
+
+
+bool BlockTAP::IsValid() const
+{
+    return header.IsValid() && data.IsValid();
+}
+
+
+void BlockTAP::Clear()
+{
+    header.Clear();
+
+    data.Clear();
+}
+
+
+bool BlockTAP::Header::Parse(wxDataInputStream &stream)
+{
+    valid = false;
+
+    if (stream.Read16() == 19)      // Для заголовка здесь всегда 19
+    {
+        if (stream.Read8() == 0)    // Чтение флага - для заголовка 0
+        {
+            stream >> type_data;
+
+            if (type_data < 4)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    file_name[i] = (char)stream.Read8();
+                }
+
+                file_name[10] = 0;
+            }
+        }
+    }
+
+    return valid;
+}
+
+
+bool BlockTAP::Data::Parse(wxDataInputStream &stream)
+{
+    valid = false;
+
+    return valid;
 }
--- sources/TAPview/ParserTAP.h
+++ sources/TAPview/ParserTAP.h
@@ -4,11 +4,54 @@
 
 
 class wxBufferedInputStream;
+class wxDataInputStream;
 
 
 struct BlockTAP
 {
+    bool Parse(wxDataInputStream &);
 
+    bool IsValid() const;
+
+    struct CommonStruct
+    {
+        uint16 size;
+
+        void Clear() { valid = false; };
+
+        bool IsValid() const { return valid; };
+
+    protected:
+
+        bool valid;
+    };
+
+    struct Header : public CommonStruct
+    {
+        uint8 type_data;    // Тип данных, следующих за заголовком
+                            // 00
+                            // 01
+                            // 02
+                            // 03 - code
+        char file_name[11];
+
+        bool Parse(wxDataInputStream &);
+
+    } header;
+
+    struct Data : public CommonStruct
+    {
+        bool Parse(wxDataInputStream &);
+
+    } data;
+
+private:
+
+    void Clear();
+
+    bool ParseHeader(wxDataInputStream &);
+
+    bool ParseData(wxDataInputStream &);
 };
 
 
@@ -19,8 +62,4 @@ public:
     void Run(wxBufferedInputStream &);
 
     std::vector<BlockTAP> blocks;
-
-private:
-
-    BlockTAP ParseBlock(wxBufferedInputStream &);
 };
