b899ea9b99448ec908a6f581e7819102f514ab6b
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2023-12-25 18:53:22 +0300

    *

--- sources/ZX-Preparator/src/Emulator/Instruction.h
+++ sources/ZX-Preparator/src/Emulator/Instruction.h
@@ -1,6 +1,7 @@
 // 2022/05/23 16:56:35 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
-#pragma warning(push, 0)
+#pragma warning(push)
+#pragma warning(disable: 4668)
 #include <vector>
 #include <string>
 #pragma warning(pop)
